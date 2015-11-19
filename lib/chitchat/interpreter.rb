c5f672e861107c3547c3a41991fdce5cc33ae775
smcho <sm.cho@mac.com>
2015-11-18 19:39:41 -0600

    gem installable modification

--- lib/chitchat/interpreter.rb
+++ lib/chitchat/interpreter.rb
@@ -1,19 +1,21 @@
-require_relative 'parser'
-require_relative 'runtime'
-require_relative 'internal'
-require_relative 'nodes_eval'
+$:.unshift File.expand_path("../", __FILE__)
+
+require 'parser'
+require 'runtime'
+require 'internal'
+require 'nodes_eval'
 
 class Interpreter
   def initialize
     @parser = Parser.new
   end
-  
+
   def eval(code, debug=false)
-      nodes = @parser.parse(code)
-      if debug
-        nodes.show
-      end
-      nodes.eval(Runtime)
+    nodes = @parser.parse(code)
+    if debug
+      nodes.show
+    end
+    nodes.eval(Runtime)
   end
 end
 
--- mygame.gemspec
+++ mygame.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.files         += ["lib/chitchat/parser.rb", "lib/chitchat/lexer.rb"]
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
